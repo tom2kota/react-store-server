43ce34d012817707b93cfa03f4a712db8617f5b0
Web Dev <tom2kota@gmail.com>
2020-11-07 21:37:36 +0200

    Fix stripe.js

--- bin/stripe.js
+++ bin/stripe.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
+// const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -11,19 +11,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cors({
-    credentials: true,
-}));
+app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//
+//     app.get('*', function (req, res) {
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+// }
 
 app.listen(port, error => {
     if (error) throw error;
@@ -39,9 +37,9 @@ app.post('/payment', (req, res) => {
 
     stripe.charges.create(body, (stripeErr, stripeRes) => {
         if (stripeErr) {
-            res.status(500).send({ error: stripeErr });
+            res.status(500).send({error: stripeErr});
         } else {
-            res.status(200).send({ success: stripeRes });
+            res.status(200).send({success: stripeRes});
         }
     });
 });
